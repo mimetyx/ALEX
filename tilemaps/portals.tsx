42cfed8873023b6e1a38a70aea4f48bbcad3c037
Brandon Corfman <bcorfman@fastmail.fm>
2023-01-22 22:25:01 -0500

    Add Tiled importer and bring in former tilemap

---
+++ tilemaps/portals.tsx
@@ -0,0 +1,60 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="portals" tilewidth="32" tileheight="32" tilecount="1152" columns="48">
+ <image source="../tiles/animations/!portal.png" width="1536" height="768"/>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="145" duration="100"/>
+   <frame tileid="289" duration="100"/>
+   <frame tileid="433" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="2">
+  <animation>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="146" duration="100"/>
+   <frame tileid="290" duration="100"/>
+   <frame tileid="434" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="49">
+  <animation>
+   <frame tileid="49" duration="100"/>
+   <frame tileid="193" duration="100"/>
+   <frame tileid="337" duration="100"/>
+   <frame tileid="481" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="50">
+  <animation>
+   <frame tileid="50" duration="100"/>
+   <frame tileid="194" duration="100"/>
+   <frame tileid="338" duration="100"/>
+   <frame tileid="482" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="97">
+  <animation>
+   <frame tileid="97" duration="100"/>
+   <frame tileid="241" duration="100"/>
+   <frame tileid="385" duration="100"/>
+   <frame tileid="529" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="98">
+  <animation>
+   <frame tileid="98" duration="100"/>
+   <frame tileid="242" duration="100"/>
+   <frame tileid="386" duration="100"/>
+   <frame tileid="530" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="145">
+  <animation>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="145" duration="100"/>
+   <frame tileid="289" duration="100"/>
+   <frame tileid="433" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
