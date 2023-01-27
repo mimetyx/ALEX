96f3cb2273767df961f19751a12a246245a99b44
Brandon Corfman <bcorfman@fastmail.fm>
2023-01-27 17:28:44 -0500

    Add initial player and camera

--- scripts/camera_move.gd
+++ scripts/camera_move.gd
@@ -8,9 +8,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var camera = $Camera2D
+	var _camera = $Camera2D
 	var player_start = $scifi_map/Locations/PlayerStart
-	var _zoof = player_start.position.x
-	camera.position.x = player_start.position.x
-	camera.position.y = player_start.position.y
-
+	var _player = $Player
+	_player.position = player_start.position
