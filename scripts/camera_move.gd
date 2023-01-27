dfc55233d750a34526129ba50be92cf06c283359
Brandon Corfman <bcorfman@fastmail.fm>
2023-01-27 17:39:37 -0500

    Move player to global position

--- scripts/camera_move.gd
+++ scripts/camera_move.gd
@@ -8,7 +8,5 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var _camera = $Camera2D
 	var player_start = $scifi_map/Locations/PlayerStart
-	var _player = $Player
-	_player.position = player_start.position
+	$Player.position = player_start.global_position
